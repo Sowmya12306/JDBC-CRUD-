1c2b6cea91fcd87c605608373cef815eb9d4f5f4
Sowmya12306 <110354797+Sowmya12306@users.noreply.github.com>
2023-06-23 02:13:49 +0530

    Update DateRetrivalApp.java

--- DateRetrivalApp.java
+++ DateRetrivalApp.java
@@ -16,25 +16,25 @@ public class DateRetrivalApp {
 		ResultSet resultSet = null;
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter the name of the user :: ");
-		String name =sc.next();		
+		String name = sc.next();		
 		String queryRead= "select name,address,gender,dob,doj,dom from userdata where name=? ";
 		try {
 			connection = PreparedStmntUtil.getConnection();
-			if(connection!=null) {
-				pstmt=connection.prepareStatement(queryRead);
-			}if(pstmt!=null) {
+			if(connection!= null) {
+				pstmt = connection.prepareStatement(queryRead);
+			}if(pstmt!= null) {
 				pstmt.setString(1, name);
-				resultSet =pstmt.executeQuery();
-			}if(resultSet!=null) {
+				resultSet = pstmt.executeQuery();
+			}if(resultSet!= null) {
 			      if(resultSet.next()) {
 			    	  System.out.println("name\taddress\tgender\tdob\tdoj\tdom");
-				String user =resultSet.getString(1);
-				String addr =resultSet.getString(2);
-				char gen =resultSet.getString(3).charAt(0);
+				String user = resultSet.getString(1);
+				String addr = resultSet.getString(2);
+				char gen = resultSet.getString(3).charAt(0);
 				SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
 				String dob = sdf.format(resultSet.getDate(4));
 				String doj = sdf.format(resultSet.getDate(5));
-				String dom =sdf.format(resultSet.getDate(6));
+				String dom = sdf.format(resultSet.getDate(6));
 				System.out.println(user + "\t"+ addr+ "\t"+gen+ "\t"+dob+ "\t"+doj+ "\t"+dom);			
 			      }else {
 			    	  System.out.println("Record not found with given name :: " + name);
